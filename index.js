370876344fa4ff27dad0cf930bb546d991456e48
HarisM <harismahjoob42@gmail.com>
2023-03-02 20:05:29 -0800

    added markdown script

---
+++ index.js
@@ -0,0 +1,178 @@
+// Node Modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markdown = require('./src/markdown');
+
+//Array of questions that will be prompted to user
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter your project description here:",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions for installation?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions for installation!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instructions for usage:',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide usage instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions on how others can contribute to the project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe the tests written for your application and how to use them:',
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('Please list any tests and how to use them!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicenses',
+        message: 'Would you like to include a license?',
+        default: false
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'What license would you like to include?',
+        choices: ['MIT', 'GPL', 'CC--0'],
+        when: ({ confirmLicenses }) => {
+            if (confirmLicenses) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+];
+
+//Writing the README
+const writeToFile = data => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('README.md', data, err => {
+            //If there is an error then reject the promise
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: console.log('Success!')
+            });
+        })
+    })
+}
+
+//Initialize
+const init = () => {
+    return inquirer.prompt(questions);
+}
+
+//Call the initialize function
+init()
+    .then(userInput => {
+        return markdown(userInput);
+    })
+    .then(readmeInfo => {
+        return writeToFile(readmeInfo);
+    })
+    .catch(err => {
+        console.log(err);
+    })
---
+++ src/markdown.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+//License badge. If no license then return empty string
+function renderLicenseBadge(license) {
+    if (!license) {
+        return ``;
+    } else {
+        return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+    }
+}
+
+//Return license link
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return `https://lbesson.mit-license.org/`
+    }
+    if (license === 'GPL') {
+        return `http://perso.crans.org/besson/LICENSE.html`
+    }
+    if (license === 'CC--0') {
+        return `https://creativecommons.org/licenses/by-nd/4.0`
+    }
+}
+
+//Render the license section. If no license then return empty string
+function renderLicenseSection(license) {
+    if (!license) {
+        return ``;
+    } else {
+        return `## Licenses
+      This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+    }
+}
+
+// Generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+    ${renderLicenseBadge(data.licenses)}
+    ## Table of Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Licenses](#licenses)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    * [Credits](#credits)
+    ## Description
+    ${data.description}
+    ## Installation
+    ${data.installation}
+    ## Usage
+    ${data.usage}
+    ${renderLicenseSection(data.licenses)}
+    ## Contributing
+    ${data.contributing}
+    ## Tests
+    ${data.tests}
+    ## Questions
+    Have questions about this project?  
+    GitHub: https://github.com/${data.github}  
+    Email: ${data.email}
+    ## Credits
+    ${data.name}
+  `;
+}
+
+module.exports = generateMarkdown;
