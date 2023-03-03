36ebc34e201a8b4d083c66177a9f56464121b071
HarisM <harismahjoob42@gmail.com>
2023-03-02 21:03:22 -0800

    fixed markdown

--- src/markdown.js
+++ src/markdown.js
@@ -28,41 +28,47 @@ function renderLicenseSection(license) {
         return ``;
     } else {
         return `## Licenses
-      This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+      This project is covered under the ${license} license.`
     }
 }
 
 // Generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
-    ${renderLicenseBadge(data.licenses)}
-    ## Table of Contents
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Licenses](#licenses)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    * [Credits](#credits)
-    ## Description
-    ${data.description}
-    ## Installation
-    ${data.installation}
-    ## Usage
-    ${data.usage}
-    ${renderLicenseSection(data.licenses)}
-    ## Contributing
-    ${data.contributing}
-    ## Tests
-    ${data.tests}
-    ## Questions
-    Have questions about this project?  
-    GitHub: https://github.com/${data.github}  
-    Email: ${data.email}
-    ## Credits
-    ${data.name}
-  `;
+function markdown(data) {
+return `# ${data.title}
+${renderLicenseBadge(data.licenses)}
+## Table of Contents
+* [Description](#description)
+* [Demonstration](#demonstration)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licenses](#licenses)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Credits](#credits)
+## Description
+${data.description}
+
+## Demonstration
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.licenses)}
+
+## Contributing
+${data.contributing}
+
+## Questions
+Have questions about this project?  
+GitHub: https://github.com/${data.github}  
+Email: ${data.email}
+## Credits
+${data.name}
+`;
 }
 
-module.exports = generateMarkdown;
+module.exports = markdown;
